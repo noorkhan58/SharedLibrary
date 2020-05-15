39c94166cfab2d800fd91c15e51c33488e05e8e5
Noor Khan <noorrezoan1988@yahoo.com>
2020-05-14 19:52:04 -0700

    test

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,27 +1,15 @@
 
-
-def check(Map params) {
-
-	checkout([
-		$class: 'GitSCM', 
-		branches: [[name: params.branch]], 
-		userRemoteConfigs: [[url: params.url]]
-		])
-}
-
-
 def call() {
 pipeline {
     agent any
     stages {
         stage('Git Checkout') {
             steps {
-                check (
-                    branch: "master",
-                    url: "https://github.com/spring-projects/spring-petclinic.git"
-                    ) 
-                    
-            	}
+                checkout([
+					$class: 'GitSCM', 
+					branches: [[name: "master"]], 
+					userRemoteConfigs: [[url: "https://github.com/spring-projects/spring-petclinic.git"]
+				])
         	}
     	}
 	}
